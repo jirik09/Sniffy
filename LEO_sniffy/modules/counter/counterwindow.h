9727bd49252888df05a3c95d4b6218006393cb70
HeyBirdie <jan.mucala@gmail.com>
2021-03-02 20:56:52 +0100

    LF DC graphics reconf fix

--- LEO_sniffy/modules/counter/counterwindow.h
+++ LEO_sniffy/modules/counter/counterwindow.h
@@ -69,6 +69,8 @@ private:
     CounterConfig *conf;
     CounterSpec *spec;
 
+    bool isAfterStart = true;
+
     void createCounterTabs(void);
     void configureCounterTabs(void);
     void createAllDisplays(void);
